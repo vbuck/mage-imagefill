58db6795cdab4b51ecbe99305c33faf148de49a8
Rick Buczynski <richard.buczynski@gmail.com>
2015-02-26 16:03:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# mage-imagefill
+# Image Fill Development Helper Module
 A Magento extension to automatically replace missing images from the page before it loads.
+
+Works by extending `Mage_Core_Model_Design_Package` to remap all invalidated skin URL requests 
+to a known placeholder image. I find this to be very useful in development environments where
+skin directories are not always synchronized with what the database might report.
---
+++ app/code/community/Vbuck/ImageFill/Model/Design/Package.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Design pacakge extensions model.
+ * 
+ * PHP Version 5
+ * 
+ * @category  Class
+ * @package   Vbuck_ImageFill
+ * @author    Rick Buczynski <me@rickbuczynski.com>
+ * @copyright 2015 Rick Buczynski. All Rights Reserved.
+ */
+
+/**
+ * Class declaration
+ *
+ * @category Class_Type_Model
+ * @package  Vbuck_ImageFill
+ * @author   Rick Buczynski <me@rickbuczynski.com>
+ */
+
+class Vbuck_ImageFill_Model_Design_Package
+    extends Mage_Core_Model_Design_Package
+{
+
+    /**
+     * Replace missing skin images with a placeholder.
+     * 
+     * @param string $file   The file path.
+     * @param array  $params Path resolution parameters.
+     * 
+     * @return string
+     */
+    public function validateFile($file, array $params)
+    {
+        $filename = parent::validateFile($file, $params);
+
+        if (!$filename) {
+            return Mage::getStoreConfig('imagefill/general/placeholder_url');
+        }
+
+        return $filename;
+    }
+
+}
